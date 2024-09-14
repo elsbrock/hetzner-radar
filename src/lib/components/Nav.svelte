d2118f60b9ab8a311f593e0da43cf965b6fa1e0a
Simon Elsbrock <simon@iodev.org>
2024-09-14 20:45:00 +0200

    fix: show analyze on mobile instead of contribute

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,32 +3,46 @@
 	import { faGithub } from '@fortawesome/free-brands-svg-icons';
   	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import { page } from '$app/stores';
-  import { faChartSimple, faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons';
+  import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
   import Radar from './Radar.svelte';
 	$: activeUrl = $page.url.pathname;
 </script>
 
-<!-- svelte-ignore missing-declaration -->
 <Navbar class="h-15 w-full border-b border-gray-200">
 	<NavBrand href="/">
-		<div style="width: 32px; height: 32px">
-			<Radar />
-		</div>
-		<p class="self-center whitespace-nowrap text-xl font-semibold
-		dark:text-white ml-5">Server Radar</p>
+			<div style="width: 32px; height: 32px">
+					<Radar />
+			</div>
+			<p class="self-center whitespace-nowrap text-xl font-semibold dark:text-white ml-5">Server Radar</p>
 	</NavBrand>
-	<div class="flex md:order-2">
-		<Button size="sm" href="https://github.com/elsbrock/hetzner-radar">
-			<FontAwesomeIcon class="w-5 h-5 me-2" icon={faGithub} /> Contribute
-		</Button>
-		<NavHamburger />
+
+	<!-- Contribute Button for Desktop -->
+	<div class="hidden md:flex md:order-2">
+			<Button size="sm" href="https://github.com/elsbrock/hetzner-radar">
+					<FontAwesomeIcon class="w-5 h-5 me-2" icon={faGithub} /> Contribute
+			</Button>
 	</div>
+
+	<!-- Analyze Button and Hamburger for Mobile -->
+	<div class="flex md:order-2 md:hidden">
+			<Button size="sm" href="/analyze">
+					<FontAwesomeIcon class="w-5 h-5 me-2" icon={faChartSimple} /> Analyze
+			</Button>
+			<NavHamburger />
+	</div>
+
 	<NavUl class="order-1" {activeUrl}>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/about">About</NavLi>
-		<NavLi href="/analyze">
-			<FontAwesomeIcon class="me-2" icon={faChartSimple} />Analyze
-		</NavLi>
-		<NavLi href="/statistics">Statistics</NavLi>
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/analyze">
+					<FontAwesomeIcon class="me-2" icon={faChartSimple} /> Analyze
+			</NavLi>
+			<NavLi href="/statistics">Statistics</NavLi>
+			<!-- Optional: Include "Contribute" in the menu -->
+			<!--
+			<NavLi href="https://github.com/elsbrock/hetzner-radar">
+					<FontAwesomeIcon class="me-2" icon={faGithub} /> Contribute
+			</NavLi>
+			-->
 	</NavUl>
 </Navbar>
