9285d2a90d9b94ee68ce5e84912355eb547798e3
Simon Elsbrock <simon@iodev.org>
2024-10-26 13:19:11 +0200

    chore: improve loading screen a little

--- src/routes/analyze/+page.svelte
+++ src/routes/analyze/+page.svelte
@@ -178,11 +178,22 @@
 </script>
 
 {#if !Number.isNaN($dbInitProgress) && $dbInitProgress < 100}
-	<div class="flex min-h-screen items-center justify-center">
-		<div class="w-[40vw]">
-			<Progressbar {$dbInitProgress} labelOutside="Loading data…" />
-		</div>
-	</div>
+<div class="flex flex-col flex-1 px-4 py-8 bg-gray-50" style="padding: 28% 0">
+  <!-- Loader Container -->
+  <div class="flex-grow flex items-center justify-center">
+    <div class="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+      <h2 class="text-xl font-semibold text-gray-700 mb-4">Hang tight</h2>
+      <Progressbar
+        progress={$dbInitProgress}
+				labelInside
+				animate={true}
+				 size="h-4"
+      />
+      <p class="mt-5 text-sm text-gray-500">Please wait while we initialize our
+      data. This should only take a couple of seconds.</p>
+    </div>
+  </div>
+</div>
 {:else if filter}
 	<div
 		class="grid min-h-screen grid-cols-1 sm:grid-cols-1 lg:grid-cols-[auto,1fr]"
