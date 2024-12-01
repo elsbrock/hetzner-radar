51d239911d1478298838be8f0f44a17316836654
Simon Elsbrock <simon@iodev.org>
2024-12-01 22:36:32 +0100

    chore(footer): add . after server radar

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,11 +1,20 @@
 <script>
-	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+    import {
+        Footer,
+        FooterCopyright,
+        FooterLink,
+        FooterLinkGroup,
+    } from "flowbite-svelte";
     const buildDate = __BUILD_STAMP__;
 </script>
 
 <Footer class="border-t">
-	<div class="p-5 sm:flex sm:items-center sm:justify-between">
-		<FooterCopyright href="/" by="Server Radar" year={new Date().getFullYear()} />
+    <div class="p-5 sm:flex sm:items-center sm:justify-between">
+        <FooterCopyright
+            href="/"
+            by="Server Radar."
+            year={new Date().getFullYear()}
+        />
         <div class="flex md:order-2">
             <FooterLinkGroup
                 ulClass="flex flex-wrap order-1 items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
@@ -16,7 +25,9 @@
             </FooterLinkGroup>
         </div>
         <div class="order-1">
-            <p class="text-xs text-gray-500 dark:text-gray-400 text-italic">Last Update {buildDate} (Europe/Berlin)</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400 text-italic">
+                Last Update {buildDate} (Europe/Berlin)
+            </p>
         </div>
-	</div>
+    </div>
 </Footer>
