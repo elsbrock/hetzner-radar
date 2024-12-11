8d460ca4db671449ac854ea28d5b9f5feba3b817
Simon Elsbrock <simon@iodev.org>
2024-12-11 13:48:22 +0100

    feat: add sitemap
    we need it for Google

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,8 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import * as sitemap from 'super-sitemap';
+
+export const GET: RequestHandler = async () => {
+  return await sitemap.response({
+    origin: 'https://radar.iodev.org',
+  });
+};
