1ffdd60538b29327d9bf69cb34882e5a68f8a597
Simon Elsbrock <simon@iodev.org>
2025-04-30 22:40:04 +0200

    fix(tests): try to get tests working

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -2,6 +2,5 @@ import test, { expect } from './fixtures';
 
 test('landing page', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Find the Best Deals on' })).toBeVisible();
-  await expect(page.getByText('Server Radar monitors Hetzner')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Stop Overpaying' })).toBeVisible();
 });
