1dd92ac2c792fd3a0f260e9aa34d3384501f8ab3
Simon Elsbrock <simon@iodev.org>
2025-05-01 23:41:44 +0200

    fix(test): no longer using apex

--- tests/analyze.spec.ts
+++ tests/analyze.spec.ts
@@ -6,8 +6,6 @@ test('analyze: we have data', async ({ page }) => {
   await expect(page.getByTestId('server-pricechart')).toBeVisible();
   await page.getByTestId('server-card').first().waitFor({ timeout: 10000});
   await expect(await page.getByTestId('server-card').count()).toBeGreaterThan(0);
-  await expect(page.getByText('Volume', { exact: true })).toBeVisible();
-  await expect(page.getByText('Price (€)', { exact: true })).toBeDefined();
   await expect(page.getByTestId('filter-clear')).not.toBeVisible();
   await page.getByTestId('filter-save').click();
   await expect(page.getByTestId('filter-clear')).toBeVisible();
